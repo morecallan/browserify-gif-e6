bf51c7a0871aaee4797023c6ae1cc5228c9c3d1c
holmanhub <holman.eric85@gmail.com>
2017-09-16 10:01:51 -0500

    gifs printing

---
+++ javascripts/data.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const printToDom = require('./dom');
+const loadGifs = require('./gifs');
+const loadCategories = require('./categories');
+
+let gifArray = [];
+
+const errorFunction = () => {
+	console.log("it broke");
+};
+
+//When Gif load
+const whenGifsLoad = function(){
+	gifArray = JSON.parse(this.responseText).gifs;
+	//TODO: Load categories
+	loadCategories(whenCategoriesLoad, errorFunction);
+};
+
+const whenCategoriesLoad = function(){
+	let categoryArray = JSON.parse(this.responseText).categories;
+	//Combine arrays
+	combineArrays(categoryArray);
+};
+
+const combineArrays = (categories) => {
+	categories.forEach((category) => {
+		gifArray.forEach((gif) => {
+			if(gif.category === category.id) {
+				gif.categoryName = category.name;
+				gif.categoryDataName = category.dataName;
+			}
+		});
+	});
+	//TODO: call the print to dom
+	printToDom(gifArray);
+};
+
+//TODO: set up 'initializer' - load gifs
+const initializer = () => {
+	loadGifs(whenGifsLoad, errorFunction);
+};
+
+module.exports = initializer;
